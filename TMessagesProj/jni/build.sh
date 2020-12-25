a619b070b22327803b69c04586b4d6212d3328b3
xerus2000 <27jf@pm.me>
2020-12-25 18:54:33 +0100

    jni/build.sh: expand setup script

--- TMessagesProj/jni/build.sh
+++ TMessagesProj/jni/build.sh
@@ -1,6 +1,21 @@
-test -d "$ANDROID_HOME" && ! test -f "$NDK" && export NDK="$ANDROID_HOME/ndk/$(ls "$ANDROID_HOME/ndk" | tail -1)"
+# prepare environment
+if ! test -f "$NDK"; then
+  ndks="${ANDROID_HOME:-$ANDROID_SDK_ROOT}/ndk"
+  test -d "$ndks" && export NDK="$ndks/$(ls "$ndks" | tail -1)"
+fi
 export NINJA_PATH="$(command -v ninja)"
+
+# prepare submodules
+cd "$(dirname $0)"
+git submodule foreach git clean -xdf
+git submodule update --init --force
+
+# build
 ./build_ffmpeg_clang.sh
 ./patch_ffmpeg.sh
 ./patch_boringssl.sh
 ./build_boringssl.sh
+
+# Examples for invoking the scripts individually
+# NDK=/opt/android/sdk/ndk/22.0.7026061 ./build_ffmpeg_clang.sh
+# NINJA_PATH=$(command -v ninja) NDK=/opt/android/sdk/ndk/22.0.7026061 ./build_boringssl.sh
